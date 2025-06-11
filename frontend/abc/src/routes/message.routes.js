38e51f91b80e57b948105ce4bf94ab21bdf13284
RjRishuSty <rishurajk59@gmail.com>
2025-06-11 20:33:32 +0530

    ho jwo

--- frontend/abc/src/routes/message.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { addMessage } from "../controllers/message.controllers.js";
-
-const router = Router();
-
-router.post('/', addMessage);
-
-export default router;
