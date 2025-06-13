1f89cfd749b0d81d8e151063d9a89b31c5636d45
RjRishuSty <rishurajk59@gmail.com>
2025-06-13 11:27:40 +0530

    update cokkioes

--- backend/src/lib/generateToken.js
+++ backend/src/lib/generateToken.js
@@ -1,17 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.SECRET_KEY, {
     expiresIn: "7d",
   });
   // Cookies............
   res.cookie("jwt_token", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 Days
     httpOnly: true,
-    sameSite: isProduction ? "none" : "lax",
-    secure: isProduction,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
   });
   return token;
 };
